75a2296428978a7c5d63d25aeae88934e56e3f6d
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-10-10 11:19:10 +0200

    props.1

--- src/components.1/PokemonCard.jsx
+++ src/components.1/PokemonCard.jsx
@@ -1,28 +1,26 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+// function PokemonCard(props) {
+//   const {name, imgSrc} = props.pokemon;
+//   return (
+//     <figure>
+//       <img src={imgSrc} alt={name}/>
+//       <figcaption>{name}</figcaption>
+//     </figure>
+//   );
+// }
 
-const pokemon = pokemonList[1];
+// export default PokemonCard;
 
-function PokemonCard() {
-    return (
-        <figure>
-            {pokemon.imgSrc ? (
-                <img src={pokemon.imgSrc} alt={pokemon.name}/>
-            ) : (
-                <p>???</p>
-            )}
-        <figcaption>{pokemon.name}</figcaption>
-        </figure>
-    );
-}
-
-
-export default PokemonCard;
+function PokemonCard(props) {
+    console.log(props);
+  
+    return <figure>
+    { props.pokemon.imgSrc !== undefined ?
+       (<img src={props.pokemon.imgSrc} alt={props.pokemon.name}/>)
+       : (<p>???</p>)
+    }
+    <figcaption>{props.pokemon.name}</figcaption>
+    </figure>;
+  }
+  
+  
+  export default PokemonCard;
