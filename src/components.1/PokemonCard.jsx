9bc8b213550aa31f64d2289794b381f31d229c22
Daaamns <145469765+Daaamns@users.noreply.github.com>
2023-10-11 14:08:00 +0200

    Update PokemonCard.jsx

--- src/components.1/PokemonCard.jsx
+++ src/components.1/PokemonCard.jsx
@@ -14,14 +14,15 @@ import PropTypes from "prop-types";
 function PokemonCard(props) {
     console.log(props);
   
-    return <figure>
+    return ( 
+        <figure>
     { props.pokemon.imgSrc !== undefined ?
        (<img src={props.pokemon.imgSrc} alt={props.pokemon.name}/>)
        : (<p>???</p>)
     }
     <figcaption>{props.pokemon.name}</figcaption>
     </figure>;
-  }
+  });
 
 PokemonCard.propTypes = {
     pokemon: PropTypes.shape({
